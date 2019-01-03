049e1a44a8842f19621bc2cd726f5545b390e7c2
arphome <45835543+arphome@users.noreply.github.com>
2019-01-02 17:22:07 -0800

    Update RuiEn.py.py

--- RuiEn.py.py
+++ RuiEn.py.py
@@ -1,28 +1,18 @@
 from tkinter import *
 
-def SnakeDraw(canvas):
-    moved = True
-    global where_am_i1
-    global snake0
-    global snake1
-    global snake2
-    global snake3
-    if moved == True:
-        snake3 = canvas.create_rectangle(10,10,60,60, fill="blue",tags="snake")
-        snake2 = canvas.create_rectangle(where_am_i2, fill="blue",tags="snake")
-        snake1 = canvas.create_rectangle(where_am_i1, fill="blue",tags="snake")
-        moved = False
+#def SnakeDraw(canvas):
+
 
 def growSnake(snakeFollow,number):
     while number > 0:
         placement = length - 1
         c = snakeFollow[placement]
-        s = canvas.create_rectangle(c, fill="blue", tags="snake")
         snakeFollow.append(c)
-        canvas.move(snakeFollow[placement], -50, 0)
+        s = canvas.create_rectangle(c, fill="red")
+        S.move(snakeFollow[placement], -50, 0)
         print(snakeFollow)
         number = number - 1
-
+        
 def MoveRectangle(event):
     moved = False
     if event.keysym == 'Up':
@@ -37,15 +27,7 @@ def MoveRectangle(event):
     elif event.keysym == 'Left':
         canvas.move("snake",-50, 0)
         moved = True
-    where_am_i1 = canvas.coords(snake0)
-    print(where_am_i1)
-    where_am_i2 = canvas.coords(snake1)
-    print(where_am_i2)
-    where_am_i3 = canvas.coords(snake2)
-    print(where_am_i3)
-    where_am_i4 = canvas.coords(snake3)
-    print(where_am_i4)
-
+    
 root=Tk()
 
 canvas = Canvas(width=500, height=500)
@@ -54,7 +36,7 @@ snake1 = ''
 snake2 = ''
 snake3 = ''
 snakeFollow = [] #snakeFollow is a list that contains coords of snake parts
-number = 2
+number = 1
 canvas.pack()
 
 canvas.bind_all('<KeyPress-Right>', MoveRectangle)
@@ -73,11 +55,8 @@ length = len(snakeFollow)
                 
 growSnake(snakeFollow,number)
 
-SnakeDraw(canvas)
+#SnakeDraw(canvas)
 
 root.mainloop()
 
 
-
-
- 
