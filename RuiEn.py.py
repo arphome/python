24e2502227e5b2e47fb094e121f636de6f831784
arphome <45835543+arphome@users.noreply.github.com>
2019-02-13 17:43:21 -0800

    Update RuiEn.py.py

--- RuiEn.py.py
+++ RuiEn.py.py
@@ -13,25 +13,32 @@ def growSnake(snakeFollow,number):
         number = number - 1
         placement = placement + 1
         snakeFollow.append(s)
-    
+        
 def appleMaker(apple):
   while apple >= 0:
-    x = random.randint(25,750)
-    y = x + 35
-    xx = random.randint(25,750)
-    yy = xx + 35
     circle = canvas.create_oval(x, xx,y, yy, outline='black', fill='red')
     apple = apple - 1
-    print (apple)
     shapes.append(circle)
-    while moved == True:
+
         
-def snakeTouchApple(x, y, x1, y1):
-    if (abs(x - x1) <= 50) and (abs(y - y1) <= 50):
-        canvas.delete(apple)
+def snakeTouchApple(x, y):
+    coords = canvas.coords(snakeFollow[0])
+    x1 = coords[0]
+    y1 = coords[1]
+    xx1 = coords[2]
+    yy1 = coords[3]
+    print (abs(x1 - x) <= 50) and (abs(y1 - y) <= 50) and (abs(xx1 - xx) <= 50) and (abs(yy1 - yy) <= 50)
+    if (abs(x1 - x) <= 50) and (abs(y1 - y) <= 50) and (abs(xx1 - xx) <= 50) and (abs(yy1 - yy) <= 50):
+        print ("apple")
+        canvas.delete(shapes[0])
+        
+
+
      
 def MoveRectangle(event):
-    moved = False
+    moved = True
+    if moved == True:
+        snakeTouchApple(x, y)
     SnakeLength = len(snakeFollow) - 1
     moveSnake = SnakeLength - 1
     while moveSnake >= 0:
@@ -42,8 +49,6 @@ def MoveRectangle(event):
         elif moveSnake == 0:
             SnakeLength = len(snakeFollow) - 1
             moveSnake = SnakeLength - 1
-        
-        
     if event.keysym == 'Up':
         canvas.move(snakeFollow[0], 0, -50)
         moved = True
@@ -55,8 +60,7 @@ def MoveRectangle(event):
         moved = True
     elif event.keysym == 'Left':
         canvas.move(snakeFollow[0],-50, 0)
-        moved = True
-            
+        moved = True       
 
 
 root=Tk()
@@ -66,7 +70,7 @@ snakeFollow = [] #snakeFollow is a list that contains coords snake canvas parts
 number = 5
 c = ""
 event = ""
-apple = 1
+apple = 0
 shapes = []
 canvas.pack()
 
@@ -78,10 +82,16 @@ canvas.bind_all('<KeyPress-Up>', MoveRectangle)
 canvas.bind_all('<KeyPress-Down>', MoveRectangle)
                 
 #Draws the head
-y1 = 250
-x1 = 300
+y1 = 0
+x1 = 50
 xx1 = x1 + 50
 yy1 = y1 + 50
+
+#Test Coords to hold the coords for the apple
+x = random.randint(25,750)
+y = x + 50
+xx = random.randint(25,750)
+yy = xx + 50
 
 s = canvas.create_rectangle(x1,xx1,y1,yy1, fill="blue",tags="snake")
 
@@ -89,7 +99,6 @@ s = canvas.create_rectangle(x1,xx1,y1,yy1, fill="blue",tags="snake")
 snakeFollow.append(s)
 
 length = len(snakeFollow)
-
 
 growSnake(snakeFollow,number)
 
@@ -99,5 +108,6 @@ appleMaker(apple)
 #SnakeDraw(canvas)
 
 root.mainloop()
+
 
 
